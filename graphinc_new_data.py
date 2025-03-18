c67f43143cd3504979b2a0f1d03c7d464d5c6f91
aidoherty <ainedoherty03@gmail.com>
2025-03-18 17:41:08 -0400

    changed csv graphing

--- graphinc_new_data.py
+++ graphinc_new_data.py
@@ -16,15 +16,12 @@ import pandas as pd
 import csv
 
 # Read the CSV file into a DataFrame
-data = pd.read_csv('tda_experiment_data.csv')
+data = pd.read_csv('normally_distributed_data.csv')
 
 # Access data in the DataFrame using column names or indexing
 #print(data['y1_value'])
 #print(data.iloc[0])  # Access first row
-plt.scatter(data['short_distance'], data['DB_scan_overlap'], color = 'green')
-plt.title('Short Distance vs Overlap Fraction')
-plt.show()
 
-plt.scatter(data['long_distance'], data['DB_scan_overlap'], color = 'blue')
-plt.title('Long Distance vs Overlap Fraction')
+plt.scatter(data['interval'], data['overlap_fraction'], color = 'blue')
+plt.title('normally distributed data stdv vs Overlap Fraction')
 plt.show()
